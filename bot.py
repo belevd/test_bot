0baa1ede1ab73b03590e3d653dbf07ff33c84fd3
belevd <55910721+belevd@users.noreply.github.com>
2019-10-26 14:06:13 +0300

    Added venv

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging, time, ephem
+import logging, time, ephem, locale
 import settings
 
+locale.setlocale(locale.LC_ALL, "russian")
+
+# Initiate logging
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', 
                     level=logging.INFO, 
                     filename='bot.log'
@@ -9,18 +12,22 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 logger = logging.getLogger(__name__)
 
+# function /start
 def greet_user(update, context):
     update.message.reply_text('Привет, {}!\nЧтобы узнать, что я умею, напиши команду /help'.format(update.message.chat.first_name))
 
+# function /help
 def help(update, context):
     update.message.reply_text('Напиши /planet *название_планеты_на_английском* и я расскажу тебе, в каком она сейчас\
     созвездии.\n\nЕще я умею повторять за тобой сообщения.\nПопробуй написать мне любой текст (кроме команд)')
 
+# function echo
 def talk_to_me(update, context):
     user_text = update.message.text
     logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, update.message.chat.id, update.message.text)
     update.message.reply_text(user_text)
 
+# function /planet
 def planet_info(update, context):
     try:
         command_text = update.message.text.split()
@@ -29,6 +36,7 @@ def planet_info(update, context):
     except:
         update.message.reply_text('Название планеты введено неправильно!')
 
+# bot "body"
 def main():
     mybot = Updater(token=settings.TOKEN, use_context=True)
 
